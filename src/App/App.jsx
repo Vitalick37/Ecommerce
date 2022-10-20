5c8fe94e77aecb680d0de5320f0c195b16c09c1f
Vitalick37 <vit198837@mail.ru>
2022-10-20 21:25:00 +0300

    add category

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import styles from './App.module.css';
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import Items from '@components/Items'
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import Items from '@components/Items';
+import Category from '@components/Category';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       orders: [],
+      currentItems: [],
       items: [
         {
           id: 1,
@@ -60,17 +62,31 @@ class App extends React.Component {
         },
       ]
     }
-    this.addToOrder = this.addToOrder.bind(this)
-    this.deleteOrder = this.deleteOrder.bind(this)
+    this.state.currentItems = this.state.items;
+    this.addToOrder = this.addToOrder.bind(this);
+    this.deleteOrder = this.deleteOrder.bind(this);
+    this.chooseCategory = this.chooseCategory.bind(this);
   }
   render() {
     return (
       <div className='wrapper'>
         <Header orders={this.state.orders} onDelete={this.deleteOrder}/>
-        <Items items = {this.state.items} onAdd={this.addToOrder} />
+        <Category chooseCategory={this.chooseCategory}/>
+        <Items items = {this.state.currentItems} onAdd={this.addToOrder} />
         <Footer />
       </div>
     );
+  }
+
+  chooseCategory(category) {
+    if(category === 'all') {
+      this.setState({currentItems: this.state.items})
+      return
+    }
+
+    this.setState({
+      currentItems: this.state.items.filter(el => el.category === category)
+    })
   }
 
   deleteOrder(id) {
